efab001ac0f50b90c3b5d183a4f3fd0476481374
raiiinewater <r2conneely@student.ccn.ac.uk>
2023-12-14 11:15:34 +0000

    blockupdate1

--- app.py
+++ app.py
@@ -7,6 +7,29 @@ width = 400
 height = 600 
 vel = 150
 
+black = (0, 0, 0)
+transparent = (0, 0, 0, 0)
+
+blue = pygame.image.load('static/CarBlockBlue.png')
+green = pygame.image.load('static/CarBlockGreen.png')
+darkorange = pygame.image.load('static/CarBlockDarkOrange.png')
+orange = pygame.image.load('static/CarBlockOrange.png')
+red = pygame.image.load('static/CarBlockRed.png')
+yellow = pygame.image.load('static/CarBlockYellow.png')
+
+all_sprites_list = pygame.sprite.Group()
+
+class Brick(pygame.sprite.Sprite): 
+    
+    def __init__(self, colour, width, height, cartype):
+        super().__init__()
+        self.cartype = cartype
+        self.image = self.cartype
+        self.image = pygame.transform.scale(self.image, [width, height])
+        self.image.set_colorkey(black)
+
+        self.rect = self.image.get_rect()
+
 bg = pygame.image.load('static/RoundBackground.png')
 char = pygame.image.load('static/PlayerCar.png')
 
@@ -34,6 +57,7 @@ SCREEN_HEIGHT = 1080
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("placeholder")
 
 # player = Player()
 
@@ -59,8 +83,46 @@ def redrawGameWindow():
     for ball in balls:
         ball.draw(screen)
 
-    pygame.display.update()
+    all_bricks = pygame.sprite.Group()
+    for i in range(14):
+        brick = Brick(transparent, 100, 60, red)
+        brick.rect.x = 80 + i* 100
+        brick.rect.y = 60
+        all_sprites_list.add(brick)
+        all_bricks.add(brick)
+    for i in range(14):
+        brick = Brick(transparent, 100, 60, darkorange)
+        brick.rect.x = 80 + i* 100
+        brick.rect.y = 100
+        all_sprites_list.add(brick)
+        all_bricks.add(brick)
+    for i in range(14):
+        brick = Brick(transparent, 100, 60, orange)
+        brick.rect.x = 80 + i* 100
+        brick.rect.y = 140
+        all_sprites_list.add(brick)
+        all_bricks.add(brick)
+    for i in range(14):
+        brick = Brick(transparent, 100, 60, yellow)
+        brick.rect.x = 80 + i* 100
+        brick.rect.y = 180
+        all_sprites_list.add(brick)
+        all_bricks.add(brick)
+    for i in range(14):
+        brick = Brick(transparent, 100, 60, green)
+        brick.rect.x = 80 + i* 100
+        brick.rect.y = 220
+        all_sprites_list.add(brick)
+        all_bricks.add(brick)
+    for i in range(14):
+        brick = Brick(transparent, 100, 60, blue)
+        brick.rect.x = 80 + i* 100
+        brick.rect.y = 260
+        all_sprites_list.add(brick)
+        all_bricks.add(brick)
     
+    all_sprites_list.draw(screen)
+    pygame.display.update()
 
 balls = []
 
@@ -106,74 +168,3 @@ while running:
 
 
     redrawGameWindow()
-
-
-import pygame
-
-x = 50
-y = 50
-width = 40
-height = 60 
-vel = 5
-
-
-
-bg = pygame.image.load('static/RoundBackground.png')
-char = pygame.image.load('static/PlayerCar.png')
-
-from pygame.locals import(
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_ESCAPE,
-    KEYDOWN,
-    QUIT,
-)
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
-
-
-# class Player(pygame.sprite.Sprite):
-#     def __init__(self):
-#         super(Player, self).__init__()
-#         self.surf = pygame.Surface((75,25))
-#         self.surf.fill((255, 255, 255))
-#         self.rect = self.surf.get_rect()
-
-
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-# player = Player()
-
-running = True 
-
-while running:
-    pygame.time.delay(100)
-    screen.blit(char, (0,0))
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-        
-            if event.key == K_ESCAPE:
-                running = False 
-        
-        elif event.type == QUIT:
-            running = False
-
-    pressed_keys = pygame.key.get_pressed()
-
-    if pressed_keys[pygame.K_LEFT] and x > vel:
-        x -= vel
-    
-    if pressed_keys[pygame.K_RIGHT] and x < 800 - vel - width:
-        x += vel 
-
-
-
-    # screen.blit(player.surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
-    screen.blit(char, (x,y))
-    pygame.display.update()
-
-
